7f904025c9f25824404266706954d74379547b57
Tony Huang <antiyourself@hotmail.com>
2020-03-18 21:51:50 +0800

    input

---
+++ Input.py
@@ -0,0 +1,8 @@
+name = input('Enter Name: ')
+print('Hi', name)
+
+height = input('How tall are you? ')
+weight = input("What's your weight? ")
+
+print('You are ', height)
+print('Your weight is ', weight)
