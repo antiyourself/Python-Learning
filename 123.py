bc21f467485b94815190f92fc40d201bb6044338
Tony Huang <antiyourself@hotmail.com>
2020-03-18 21:47:55 +0800

    print

---
+++ 123.py
@@ -0,0 +1,11 @@
+#1. 整數 integer int 
+#2. 浮點數 float
+#3. 布林值 boolean bool(True, False)
+#4. 字串 string str (文字) 'Tony'
+
+price = 10
+pen = 0.38
+rain = True
+name = 'Tony'
+
+print(price,  pen, rain, name)
